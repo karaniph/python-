4ff790fbc328ce50de8984b81ab0792a0a27485c
karaniph <127226766+karaniph@users.noreply.github.com>
2023-07-18 12:48:12 +0300

    divide chocolates among children.py

--- # take two integer inputs for chocolates.py
+++ # take two integer inputs for chocolates.py
@@ -9,3 +9,12 @@ print(number_of_chocolates)
 # find remaining chocolates and print it
 remaining_chocolates = chocolates % children
 print(remaining_chocolates)
+
+
+
+Create a program to divide N chocolates among M children.
+
+Get an integer value for the number of chocolates and assign it to the chocolates variable.
+Get an integer input for the number of children and assign it to the children variable.
+Find the number of chocolate each child gets after division and print it.
+Find the number of remaining chocolates and print it.
